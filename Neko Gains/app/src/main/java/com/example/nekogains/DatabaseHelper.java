66bbf0453b898ab8434df5aeecf3ea873ad00c6d
Roy Lu <roy.lu@mail.utoronto.ca>
2020-02-14 11:01:55 -0500

    THEC-33 Populated database with workouts

--- Neko Gains/app/src/main/java/com/example/nekogains/DatabaseHelper.java
+++ Neko Gains/app/src/main/java/com/example/nekogains/DatabaseHelper.java
@@ -11,6 +11,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
     public static final String COL_3 = "CALORIES";
+    public static final String[] workouts = {"Push-Ups", "Bench-Dips", "Chin-Ups", "Squats", "Lunges", "Calf-Raises", "Planks", "Sit-Ups", "Leg Raises", "Running", "Burpees", "Jumping Jacks"};
+    public static final double[] calories = {1, 3, 1, 14, 0.9, 0.3, 3.5, 0.3, 0.7, 15, 13, 9};
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -21,6 +23,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY, NAME TEXT, CALORIES INTEGER);");
+        ContentValues contentValues = new ContentValues();
+        for (int i = 0; i < workouts.length; i++) {
+            contentValues.put(COL_2, workouts[i]);
+            contentValues.put(COL_3, calories[i]);
+            db.insert(TABLE_NAME, null, contentValues);
+        }
     }
 
     @Override
@@ -28,7 +36,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 
-    public boolean insertData(String name, String calories) {
+    public boolean insertData(String name, float calories) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, name);
