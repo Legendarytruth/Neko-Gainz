443a70cb22b8b3e2f246f84b5e5c81536548c5fa
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-02-19 17:34:24 -0500

    THEC-5 completed layout for progress/stats page

--- Neko Gains/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ Neko Gains/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f080041;
-        public static final int gone = 0x7f08004f;
-        public static final int invisible = 0x7f080059;
-        public static final int left = 0x7f08005e;
-        public static final int packed = 0x7f080077;
-        public static final int parent = 0x7f080079;
-        public static final int percent = 0x7f08007c;
-        public static final int right = 0x7f080081;
-        public static final int spread = 0x7f0800a4;
-        public static final int spread_inside = 0x7f0800a5;
-        public static final int start = 0x7f0800aa;
-        public static final int top = 0x7f0800c0;
-        public static final int wrap = 0x7f0800cf;
+        public static final int bottom = 0x7f090025;
+        public static final int end = 0x7f090041;
+        public static final int gone = 0x7f090050;
+        public static final int invisible = 0x7f09005a;
+        public static final int left = 0x7f090060;
+        public static final int packed = 0x7f090079;
+        public static final int parent = 0x7f09007b;
+        public static final int percent = 0x7f090080;
+        public static final int right = 0x7f090094;
+        public static final int spread = 0x7f0900b8;
+        public static final int spread_inside = 0x7f0900b9;
+        public static final int start = 0x7f0900be;
+        public static final int top = 0x7f0900d4;
+        public static final int wrap = 0x7f0900e6;
     }
     public static final class styleable {
         private styleable() {}
--- Neko Gains/app/src/main/java/com/example/nekogains/ProgressFrag.java
+++ Neko Gains/app/src/main/java/com/example/nekogains/ProgressFrag.java
@@ -14,6 +14,7 @@ public class ProgressFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.progress_frag, container, false);
     }
 }
