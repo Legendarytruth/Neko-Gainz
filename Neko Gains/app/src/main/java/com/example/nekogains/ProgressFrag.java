d97c1bf550b427316162fbe391b5571ff5a7da08
Chencen Liang <coco.liang@mail.utoronto.ca>
2020-03-11 20:10:20 -0400

    THEC-11 layout complete for progress page

--- Neko Gains/app/src/main/java/com/example/nekogains/ProgressFrag.java
+++ Neko Gains/app/src/main/java/com/example/nekogains/ProgressFrag.java
@@ -17,22 +17,28 @@ public class ProgressFrag extends Fragment {
 
     private LineGraphSeries<DataPoint> series;
     private View view;
+    private User user;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        MainActivity activity = (MainActivity) getActivity();
+        user = new User(DatabaseHelper.getInstance(MainActivity.getContext()), ((MainActivity)this.getActivity()).getUserId());
+        /*int last = user.getLastDay();
+        String exerciseType = user.getUserCalendarData(last, "WARMUP");
+        int data = Integer.parseInt(user.getUserCalendarData(last, exerciseType));*/
         view = inflater.inflate(R.layout.progress_frag, container, false);
-        GraphView graphView = view.findViewById(R.id.graph);
+        //GraphView graphView = view.findViewById(R.id.graph);
         double x,y;
-        x = 0.0;
+            series = new LineGraphSeries<>();
+        int numDataPoints = 14;
+        //x = last;
+        //y = data;
+        x = 1;
+        y = 1;
+        series.appendData(new DataPoint(x,y), true, 14);
 
-        series = new LineGraphSeries<>();
-        int numDataPoints = 100;
-        for (int i = 0; i <numDataPoints; i++){
-            x = x + 5;
-            y = x+ 2;
-            series.appendData(new DataPoint(x,y), true, 100);
-        }
-        graphView.addSeries(series);
+        //graphView.addSeries(series);
         return view;
     }
 }
